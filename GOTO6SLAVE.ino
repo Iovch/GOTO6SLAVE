904bdb47fbd1780b8666c3fc1758dbb3b2a937d0
Iovch <38076694+Iovch@users.noreply.github.com>
2018-04-05 23:34:40 +0500

    Add files via upload

---
+++ GOTO6SLAVE.ino
@@ -0,0 +1,40 @@
+// GOTO6SLAVE.ino Written by Igor Ovchinnikov 30/07/2016
+
+#include <IRremote.h>
+#include <Wire.h>
+
+#define RECV_PIN   12 //Пин ИК приемника
+#define SLAVE_ADDR 10 //Адрес I2C Slave устройства
+
+IRrecv irrecv(RECV_PIN); //Создаем обработчик ИК сигнала
+decode_results results;  //Переменная, хранящая результат
+
+unsigned int iLastValue=0;
+boolean bDebug=false;
+
+//Этот файл должен находиться в той же директории, что основной GOTO6SLAVE.ino:
+
+#include "IRduino.h"
+
+void ReqEvent(void)
+{
+ Wire.write(iLastValue);
+ if(bDebug) Serial.println(iLastValue); //Дублируем данные в СОМ порт при отладке
+}
+
+void setup()
+{
+  Wire.begin(SLAVE_ADDR);    // Создаем I2C Slave устройство
+  Wire.onRequest(ReqEvent);  // Регистрируем функцию обработки запроса I2C
+  if(bDebug) {Serial.begin(9600); Serial.flush();} // Подготавливаем СОМ порт для отладки
+  irrecv.enableIRIn();       // Начинаем прием
+}
+
+void loop()
+{
+ unsigned int iValue=0;
+ iValue=AskIRduino(); // Опрос 17 кнопочного пульта от Ардуино
+ if(iValue!=50) iLastValue=iValue;
+ if(bDebug){ ReqEvent(); }
+ delay(125);
+}
---
+++ IRduino.h
@@ -0,0 +1,44 @@
+// IRduino.h Written by Igor Ovchinnikov 21/07/2016
+// Команды штатного 17-ти кнопочного ИК пульта Ардуино
+// скучновато
+// #include <IRremote.h> //В заголовок основного файла
+
+//int RECV_PIN = 12;        //В заголовок основного файла
+//IRrecv irrecv(RECV_PIN);  //В заголовок основного файла
+//decode_results results;   //В заголовок основного файла
+
+//irrecv.enableIRIn(); // Это в Setup
+
+unsigned int AskIRduino(void)
+{
+ unsigned int ulRetVal=0;
+ if (irrecv.decode(&results))
+  { 
+    switch(results.value)
+     {
+      case 16736925: {ulRetVal=13; break;} // "^"
+      case 16720605: {ulRetVal=14; break;} // "<"
+      case 16712445: {ulRetVal=15; break;} // "Ok"
+      case 16761405: {ulRetVal=16; break;} // ">"
+      case 16754775: {ulRetVal=17; break;} // "v"
+  
+      case 16738455: {ulRetVal= 1; break;} // "1"
+      case 16750695: {ulRetVal= 2; break;} // "2"
+      case 16756815: {ulRetVal= 3; break;} // "3"
+      case 16724175: {ulRetVal= 4; break;} // "4"
+      case 16718055: {ulRetVal= 5; break;} // "5"
+      case 16743045: {ulRetVal= 6; break;} // "6"
+      case 16716015: {ulRetVal= 7; break;} // "7"
+      case 16726215: {ulRetVal= 8; break;} // "8"
+      case 16734885: {ulRetVal= 9; break;} // "9"
+      case 16730805: {ulRetVal=10; break;} // "0"
+      
+      case 16728765: {ulRetVal=11; break;} // "*"
+      case 16732845: {ulRetVal=12; break;} // "#"
+      case 4294967295: {ulRetVal=50; break;} // "Cycle"
+     }
+//    ulRetVal=results.value;
+    irrecv.resume();
+  }
+ return ulRetVal;
+}
